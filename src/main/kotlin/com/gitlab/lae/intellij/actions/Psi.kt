1e4e707c45e872029093046570bb1922b2d34075
Lae <laec@protonmail.ch>
2018-09-02 20:50:17 +1200

    Support more structures.

--- src/main/kotlin/com/gitlab/lae/intellij/actions/Psi.kt
+++ src/main/kotlin/com/gitlab/lae/intellij/actions/Psi.kt
@@ -7,6 +7,7 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.actionSystem.EditorWriteActionHandler
 import com.intellij.openapi.editor.actions.TextComponentEditorAction
 import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiModifierListOwner
 import com.intellij.psi.PsiStatement
 import com.intellij.psi.PsiWhiteSpace
 
@@ -15,6 +16,19 @@ class PsiDelete : TextComponentEditorAction(object : EditorWriteActionHandler(tr
     override fun executeWriteAction(editor: Editor, caret: Caret?, ctx: DataContext) {
         caret ?: return
 
+        val doc = editor.document
+        if (!doc.isWritable) {
+            return
+        }
+
+        val line = doc.getLineNumber(caret.offset)
+        if (doc.getLineEndOffset(line) == caret.offset) {
+            if (line + 1 < doc.lineCount) {
+                doc.deleteString(caret.offset, doc.getLineStartOffset(line + 1))
+            }
+            return
+        }
+
         var element = ctx.getData(PSI_FILE)?.findElementAt(caret.offset)
                 ?: return
 
@@ -22,14 +36,14 @@ class PsiDelete : TextComponentEditorAction(object : EditorWriteActionHandler(tr
             element = element.nextSibling
         }
 
-        while (element !is PsiStatement) {
+        while (element !is PsiStatement && element !is PsiModifierListOwner) {
             element = element.parent
             if (element is PsiFile) {
                 return
             }
         }
 
-        editor.document.deleteString(caret.offset, element.textRange.endOffset)
+        doc.deleteString(caret.offset, element.textRange.endOffset)
     }
 
 })
--- src/test/kotlin/com/gitlab/lae/intellij/actions/PsiDeleteTest.kt
+++ src/test/kotlin/com/gitlab/lae/intellij/actions/PsiDeleteTest.kt
@@ -47,14 +47,276 @@ class PsiDeleteTest : LightPlatformCodeInsightFixtureTestCase() {
         """.trimIndent(), LogicalPosition(2, 7))
     }
 
+    fun `test delete complete try-catch`() {
+        test("""
+            class Main {
+              public static int test() {
+                try {
+                  throw new RuntimeException();
+                } catch (RuntimeException e) {
+                }
+              }
+            }
+        """.trimIndent(), """
+            class Main {
+              public static int test() {
+
+              }
+            }
+        """.trimIndent(), LogicalPosition(2, 0))
+    }
+
+    fun `test delete partial try-catch`() {
+        test("""
+            class Main {
+              public static int test() {
+                try {
+                  throw new RuntimeException();
+                } catch (RuntimeException e) {
+                }
+              }
+            }
+        """.trimIndent(), """
+            class Main {
+              public static int test() {
+                tr
+              }
+            }
+        """.trimIndent(), LogicalPosition(2, 6))
+    }
+
+    fun `test delete complete return statement`() {
+        test("""
+            class Main {
+              public static int test() {
+                return
+                    1;
+              }
+            }
+        """.trimIndent(), """
+            class Main {
+              public static int test() {
+
+              }
+            }
+        """.trimIndent(), LogicalPosition(2, 0))
+    }
+
+    fun `test delete partial return statement`() {
+        test("""
+            class Main {
+              public static int test() {
+                return
+                    1;
+              }
+            }
+        """.trimIndent(), """
+            class Main {
+              public static int test() {
+                retur
+              }
+            }
+        """.trimIndent(), LogicalPosition(2, 9))
+    }
+
+    fun `test delete complete method`() {
+        test("""
+            class Main {
+              public static int test() {
+                return 1;
+              }
+            }
+        """.trimIndent(), """
+            class Main {
+
+            }
+        """.trimIndent(), LogicalPosition(1, 0))
+    }
+
+    fun `test delete partial method`() {
+        test("""
+            class Main {
+              public static int test() {
+                return
+                    1;
+              }
+            }
+        """.trimIndent(), """
+            class Main {
+              public stat
+            }
+        """.trimIndent(), LogicalPosition(1, 13))
+    }
+
+    fun `test delete complete constructor`() {
+        test("""
+            class Main {
+              int i;
+              public Main() {
+                i = 1;
+              }
+            }
+        """.trimIndent(), """
+            class Main {
+              int i;
+
+            }
+        """.trimIndent(), LogicalPosition(2, 0))
+    }
+
+    fun `test delete partial constructor`() {
+        test("""
+            class Main {
+              int i;
+              public Main() {
+                i = 1;
+              }
+            }
+        """.trimIndent(), """
+            class Main {
+              int i;
+              public Ma
+            }
+        """.trimIndent(), LogicalPosition(2, 11))
+    }
+
+    fun `test delete complete initializer`() {
+        test("""
+            class Main {
+              int i;
+              {
+                i = 1;
+              }
+            }
+        """.trimIndent(), """
+            class Main {
+              int i;
+
+            }
+        """.trimIndent(), LogicalPosition(2, 0))
+    }
+
+    fun `test delete partial initializer`() {
+        test("""
+            class Main {
+              static int i;
+              static {
+                i = 1;
+              }
+            }
+        """.trimIndent(), """
+            class Main {
+              static int i;
+              stati
+            }
+        """.trimIndent(), LogicalPosition(2, 7))
+    }
+
+    fun `test delete complete field`() {
+        test("""
+            class Main {
+              public static int test = 1;
+            }
+        """.trimIndent(), """
+            class Main {
+
+            }
+        """.trimIndent(), LogicalPosition(1, 0))
+    }
+
+    fun `test delete partial field`() {
+        test("""
+            class Main {
+              public static int test = 1;
+            }
+        """.trimIndent(), """
+            class Main {
+              public stat
+            }
+        """.trimIndent(), LogicalPosition(1, 13))
+    }
+
+    fun `test delete complete inner class`() {
+        test("""
+            class Main {
+              public static class Test {
+                int i;
+              }
+            }
+        """.trimIndent(), """
+            class Main {
+
+            }
+        """.trimIndent(), LogicalPosition(1, 0))
+    }
+
+    fun `test delete partial inner class`() {
+        test("""
+            class Main {
+              public static class Test {
+                int i;
+              }
+            }
+        """.trimIndent(), """
+            class Main {
+              public stat
+            }
+        """.trimIndent(), LogicalPosition(1, 13))
+    }
+
+    fun `test delete complete class`() {
+        test("""
+            class Main {
+              public static int test = 1;
+            }
+        """.trimIndent(), """
+
+        """.trimIndent(), LogicalPosition(0, 0))
+    }
+
+    fun `test delete partial class`() {
+        test("""
+            class Main {
+              public static int test = 1;
+            }
+        """.trimIndent(), """
+            class Ma
+        """.trimIndent(), LogicalPosition(0, 8))
+    }
+
+    fun `test delete empty line`() {
+        test("""
+            class Main {
+
+              public static int test = 1;
+            }
+        """.trimIndent(), """
+            class Main {
+              public static int test = 1;
+            }
+        """.trimIndent(), LogicalPosition(1, 0))
+    }
+
+    fun `test delete text end does nothing`() {
+        test("""
+            class Main {
+              public static int test = 1;
+            }
+        """.trimIndent(), """
+            class Main {
+              public static int test = 1;
+            }
+        """.trimIndent(), LogicalPosition(2, 1))
+    }
+
     private fun test(
             initialText: String,
             expectedText: String,
-            initialCaretPosition: LogicalPosition
+            vararg initialCaretPositions: LogicalPosition
     ) {
         myFixture.configureByText(JavaFileType.INSTANCE, initialText)
-        myFixture.editor.caretModel.caretsAndSelections = listOf(CaretState(
-                initialCaretPosition, initialCaretPosition, initialCaretPosition))
+        myFixture.editor.caretModel.caretsAndSelections = initialCaretPositions
+                .map { CaretState(it, it, it) }
         myFixture.performEditorAction("com.gitlab.lae.intellij.actions.PsiDelete")
         assertEquals(expectedText, myFixture.editor.document.text)
     }
