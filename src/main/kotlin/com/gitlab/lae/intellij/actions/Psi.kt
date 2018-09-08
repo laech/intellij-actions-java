d03a66bc10f820b96b535227d0b03c256ae62d25
Lae <laec@protonmail.ch>
2018-09-08 13:34:28 +1200

    Refactor PsiKill and add more tests.

--- src/main/kotlin/com/gitlab/lae/intellij/actions/Psi.kt
+++ src/main/kotlin/com/gitlab/lae/intellij/actions/Psi.kt
@@ -2,62 +2,103 @@ package com.gitlab.lae.intellij.actions
 
 import com.intellij.openapi.actionSystem.CommonDataKeys.PSI_FILE
 import com.intellij.openapi.actionSystem.DataContext
-import com.intellij.openapi.editor.Caret
-import com.intellij.openapi.editor.Document
-import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.*
+import com.intellij.openapi.editor.EditorModificationUtil.deleteSelectedTextForAllCarets
 import com.intellij.openapi.editor.actionSystem.EditorWriteActionHandler
 import com.intellij.openapi.editor.actions.TextComponentEditorAction
 import com.intellij.psi.*
 import com.intellij.psi.util.parentOfType
 
-class PsiDelete : TextComponentEditorAction(object : EditorWriteActionHandler(true) {
-
+class PsiKill : TextComponentEditorAction(object : EditorWriteActionHandler(false) {
     override fun executeWriteAction(editor: Editor, caret: Caret?, ctx: DataContext) {
-        caret ?: return
-
-        val doc = editor.document
-        if (!doc.isWritable) {
-            return
-        }
-
-        val line = doc.getLineNumber(caret.offset)
-        if (doc.getLineEndOffset(line) == caret.offset) {
-            if (line + 1 < doc.lineCount) {
-                doc.deleteString(caret.offset, doc.getLineStartOffset(line + 1))
-            }
-            return
-        }
-
-        var element = ctx.getData(PSI_FILE)?.findElementAt(caret.offset)
-                ?: return
-
-        while (element is PsiWhiteSpace) {
-            element = element.nextSibling
-        }
-
-        while (element !is PsiStatement && element !is PsiModifierListOwner) {
-            element = element.parent
-            if (element is PsiFile) {
-                return
-            }
-        }
-
-        when (element) {
-            is PsiParameter -> deleteList(doc, caret.offset, element, PsiParameterList::getParameters)
-            is PsiTypeParameter -> deleteList(doc, caret.offset, element, PsiTypeParameterList::getTypeParameters)
-            else -> doc.deleteString(caret.offset, element.textRange.endOffset)
-        }
+        selectElementsUnderCarets(editor, ctx)
+        EditorCopyPasteHelper.getInstance().copySelectionToClipboard(editor)
+        deleteSelectedTextForAllCarets(editor)
     }
-
-    private inline fun <reified T : PsiElement> deleteList(
-            doc: Document,
-            startOffset: Int,
-            element: PsiElement,
-            getParameters: (T) -> Array<out PsiElement>
-    ) {
-        val list = element.parentOfType<T>() ?: return
-        val last = getParameters(list).lastOrNull() ?: return
-        doc.deleteString(startOffset, last.textRange.endOffset)
-    }
-
 })
+
+private fun selectElementsUnderCarets(editor: Editor, ctx: DataContext) {
+    val file = ctx.getData(PSI_FILE) ?: return
+    editor.caretModel.caretsAndSelections =
+            editor.caretModel.caretsAndSelections
+                    .mapNotNull { it.caretPosition }
+                    .map { select(editor, it, file) }
+}
+
+private fun select(editor: Editor, pos: LogicalPosition, file: PsiFile): CaretState {
+
+    if (pos.isAtEndOfLine(editor)) {
+        if (!pos.isAtLastLine(editor)) {
+            return pos.selectToNextLineStart()
+        }
+        return pos.asCaret()
+    }
+
+    val doc = editor.document
+    var element = file.findElementAt(pos.offset(editor))
+            ?: return pos.selectToLineEnd(editor)
+
+    while (element is PsiWhiteSpace) {
+        element = element.nextSibling
+    }
+
+    while (element !is PsiStatement && element !is PsiModifierListOwner) {
+        element = element.parent
+        if (element is PsiFile) {
+            return pos.selectToLineEnd(editor)
+        }
+    }
+
+    return when (element) {
+        is PsiParameter -> selectList(doc, pos, element, PsiParameterList::getParameters)
+        is PsiTypeParameter -> selectList(doc, pos, element, PsiTypeParameterList::getTypeParameters)
+        else -> pos.selectToElementEnd(editor, element)
+    }
+}
+
+private inline fun <reified T : PsiElement> selectList(
+        doc: Document,
+        pos: LogicalPosition,
+        element: PsiElement,
+        params: (T) -> Array<out PsiElement>
+): CaretState {
+
+    val list = element.parentOfType<T>()
+            ?: return pos.asCaret()
+
+    val endOffset = params(list).lastOrNull()?.textRange?.endOffset
+            ?: return pos.asCaret()
+
+    val endLine = doc.getLineNumber(endOffset)
+    val endLineColumn = endOffset - doc.getLineStartOffset(endLine)
+
+    return CaretState(pos, pos, LogicalPosition(endLine, endLineColumn))
+}
+
+private fun LogicalPosition.isAtEndOfLine(editor: Editor) =
+        editor.document.getLineEndOffset(line) -
+                editor.document.getLineStartOffset(line) == column
+
+private fun LogicalPosition.isAtLastLine(editor: Editor) =
+        line + 1 >= editor.document.lineCount
+
+private fun LogicalPosition.selectToLineEnd(editor: Editor) =
+        CaretState(this, this, LogicalPosition(line,
+                editor.document.getLineEndOffset(line)))
+
+private fun LogicalPosition.selectToNextLineStart() =
+        CaretState(this, this, LogicalPosition(line + 1, 0))
+
+private fun LogicalPosition.selectToElementEnd(
+        editor: Editor, element: PsiElement
+): CaretState {
+    val endOffset = element.textRange.endOffset
+    val endLine = editor.document.getLineNumber(endOffset)
+    val endColumn = endOffset - editor.document.getLineStartOffset(endLine)
+    return CaretState(this, this, LogicalPosition(endLine, endColumn))
+}
+
+private fun LogicalPosition.asCaret() = CaretState(this, null, null)
+
+private fun LogicalPosition.offset(editor: Editor) =
+        editor.document.getLineStartOffset(line) + column
--- src/test/kotlin/com/gitlab/lae/intellij/actions/PsiKillTest.kt
+++ src/test/kotlin/com/gitlab/lae/intellij/actions/PsiKillTest.kt
@@ -1,11 +1,12 @@
 package com.gitlab.lae.intellij.actions
 
 import com.intellij.ide.highlighter.JavaFileType
+import com.intellij.openapi.actionSystem.IdeActions
 import com.intellij.openapi.editor.CaretState
 import com.intellij.openapi.editor.LogicalPosition
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase
 
-class PsiDeleteTest : LightPlatformCodeInsightFixtureTestCase() {
+class PsiKillTest : LightPlatformCodeInsightFixtureTestCase() {
 
     fun `test delete complete statement`() {
         test("""
@@ -349,7 +350,74 @@ class PsiDeleteTest : LightPlatformCodeInsightFixtureTestCase() {
         """.trimIndent(), LogicalPosition(2, 1))
     }
 
+    fun `test multiple cursors`() {
+        test("""
+            class Main {
+              public static int test1 = 1;
+              public static int test2 = 2;
+              public static int test3() {
+                return 3;
+              }
+            }
+        """.trimIndent(), """
+            class Main {
+
+              public static int test2 = 2;
+
+            }
+        """.trimIndent(),
+                LogicalPosition(1, 0),
+                LogicalPosition(3, 0))
+    }
+
+    fun `test paste`() {
+        test(true, """
+            class Main {
+              public static int test() {
+                return 1;
+              }
+            }
+        """.trimIndent(), """
+            class Main {
+              public static int test() {
+                return 1;
+              }
+            }
+        """.trimIndent(), LogicalPosition(1, 0))
+    }
+
+    fun `test paste multiple cursors`() {
+        test(true, """
+            class Main {
+              public static int test1 = 1;
+              public static int test2 = 2;
+              public static int test3() {
+                return 3;
+              }
+            }
+        """.trimIndent(), """
+            class Main {
+              public static int test1 = 1;
+              public static int test2 = 2;
+              public static int test3() {
+                return 3;
+              }
+            }
+        """.trimIndent(),
+                LogicalPosition(1, 0),
+                LogicalPosition(3, 0))
+    }
+
     private fun test(
+            initialText: String,
+            expectedText: String,
+            vararg initialCaretPositions: LogicalPosition
+    ) {
+        test(false, initialText, expectedText, *initialCaretPositions)
+    }
+
+    private fun test(
+            paste: Boolean,
             initialText: String,
             expectedText: String,
             vararg initialCaretPositions: LogicalPosition
@@ -357,7 +425,10 @@ class PsiDeleteTest : LightPlatformCodeInsightFixtureTestCase() {
         myFixture.configureByText(JavaFileType.INSTANCE, initialText)
         myFixture.editor.caretModel.caretsAndSelections = initialCaretPositions
                 .map { CaretState(it, it, it) }
-        myFixture.performEditorAction("com.gitlab.lae.intellij.actions.PsiDelete")
+        myFixture.performEditorAction("com.gitlab.lae.intellij.actions.PsiKill")
+        if (paste) {
+            myFixture.performEditorAction(IdeActions.ACTION_EDITOR_PASTE_SIMPLE)
+        }
         assertEquals(expectedText, myFixture.editor.document.text)
     }
 }
